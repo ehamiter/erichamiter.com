c9afc213a81515afbbbc22d35710247a71a54b0f
Eric Hamiter <ehamiter@gmail.com>
2023-12-31 15:49:56 -0600

    Update some js

--- static/js/site.js
+++ static/js/site.js
@@ -9,30 +9,22 @@ function resizeCanvas() {
                        window.innerHeight);
 
   draw();
-
 }
-
-window.addEventListener('load', resizeCanvas);
-window.addEventListener('resize', resizeCanvas);
 
 function draw() {
   var canvas = document.getElementById("canvasBackground");
+  if (!canvas) return;
+
   var ctx = canvas.getContext("2d");
-
-  // Apply a blur effect
-  ctx.filter = 'blur(99px)'; // Adjust the pixel value to increase/decrease blur
-
-  // Background color
-  ctx.fillStyle = '#78909C'; // cool grey/green/blue color
+  ctx.filter = 'blur(99px)';
+  ctx.fillStyle = '#78909C';
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
   for (let i = 0; i < 20; i++) {
     var x = Math.random() * canvas.width;
     var y = Math.random() * canvas.height;
-    var size = Math.random() * 500; // Larger size for overlap
+    var size = Math.random() * 500;
     var color = `hsla(${100 + Math.random()*100}, 40%, 20%, 0.5)`;
-
-
     ctx.fillStyle = color;
 
     switch(Math.floor(Math.random() * 3)) {
@@ -54,8 +46,7 @@ function draw() {
         break;
     }
 
-    // Fine geometric lines
-    ctx.strokeStyle = '#607D8B'; // Darker shade for contrast
+    ctx.strokeStyle = '#607D8B';
     ctx.lineWidth = 2;
     ctx.beginPath();
     ctx.moveTo(Math.random() * canvas.width, Math.random() * canvas.height);
@@ -63,18 +54,13 @@ function draw() {
     ctx.stroke();
   }
 
-  // Reset filter to none after drawing
   ctx.filter = 'none';
 }
 
 window.addEventListener('load', resizeCanvas);
 window.addEventListener('resize', resizeCanvas);
 
-
-
-tippy('.link',{
-  placement: 'bottom'
-})
+tippy('.link', { placement: 'bottom' });
 
 const toggles = document.querySelectorAll('.js-change-theme');
 const body = document.querySelector('body');
@@ -83,15 +69,11 @@ const profile = document.getElementById('profile');
 toggles.forEach(toggle => {
   toggle.addEventListener('click', () => {
     if (body.classList.contains('text-gray-900')) {
-      body.classList.remove('text-gray-900');
-      body.classList.add('text-gray-100');
-      profile.classList.remove('bg-white');
-      profile.classList.add('bg-gray-900');
+      body.classList.replace('text-gray-900', 'text-gray-100');
+      profile.classList.replace('bg-white', 'bg-gray-900');
     } else {
-      body.classList.remove('text-gray-100');
-      body.classList.add('text-gray-900');
-      profile.classList.remove('bg-gray-900');
-      profile.classList.add('bg-white');
+      body.classList.replace('text-gray-100', 'text-gray-900');
+      profile.classList.replace('bg-gray-900', 'bg-white');
     }
   });
 });
