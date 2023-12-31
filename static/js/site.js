bfe63f26f9258f5140f40b42d04ba4aa34b05ec0
Eric Hamiter <ehamiter@gmail.com>
2023-12-31 14:47:30 -0600

    Replace bg pic with random canvas elements

--- static/js/site.js
+++ static/js/site.js
@@ -1,12 +1,76 @@
-function showTheRightBackgroundOnTheRightDevice() {
-  let isPortrait = window.matchMedia("(orientation: portrait)").matches;
-  let backgroundImage = isPortrait ? './static/images/bg-portrait.jpg' : './static/images/bg.jpg';
-  document.querySelector('.bg-image').style.backgroundImage = `url('${backgroundImage}')`;
+function resizeCanvas() {
+  var canvas = document.getElementById("canvasBackground");
+  var body = document.body;
+  var html = document.documentElement;
+
+  canvas.width = window.innerWidth;
+  canvas.height = Math.max(body.scrollHeight, body.offsetHeight,
+                       html.clientHeight, html.scrollHeight, html.offsetHeight,
+                       window.innerHeight);
+
+  draw();
+
 }
-document.addEventListener('DOMContentLoaded', function() {
-  showTheRightBackgroundOnTheRightDevice();
-  window.addEventListener("resize", showTheRightBackgroundOnTheRightDevice);
-});
+
+window.addEventListener('load', resizeCanvas);
+window.addEventListener('resize', resizeCanvas);
+
+function draw() {
+  var canvas = document.getElementById("canvasBackground");
+  var ctx = canvas.getContext("2d");
+
+  // Apply a blur effect
+  ctx.filter = 'blur(99px)'; // Adjust the pixel value to increase/decrease blur
+
+  // Background color
+  ctx.fillStyle = '#78909C'; // cool grey/green/blue color
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  for (let i = 0; i < 20; i++) {
+    var x = Math.random() * canvas.width;
+    var y = Math.random() * canvas.height;
+    var size = Math.random() * 500; // Larger size for overlap
+    var color = `hsla(${100 + Math.random()*100}, 40%, 20%, 0.5)`;
+
+
+    ctx.fillStyle = color;
+
+    switch(Math.floor(Math.random() * 3)) {
+      case 0:
+        ctx.fillRect(x, y, size, size);
+        break;
+      case 1:
+        ctx.beginPath();
+        ctx.arc(x, y, size / 2, 0, 2 * Math.PI);
+        ctx.fill();
+        break;
+      case 2:
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+        ctx.lineTo(x + size, y);
+        ctx.lineTo(x + size / 2, y - size);
+        ctx.closePath();
+        ctx.fill();
+        break;
+    }
+
+    // Fine geometric lines
+    ctx.strokeStyle = '#607D8B'; // Darker shade for contrast
+    ctx.lineWidth = 2;
+    ctx.beginPath();
+    ctx.moveTo(Math.random() * canvas.width, Math.random() * canvas.height);
+    ctx.lineTo(Math.random() * canvas.width, Math.random() * canvas.height);
+    ctx.stroke();
+  }
+
+  // Reset filter to none after drawing
+  ctx.filter = 'none';
+}
+
+window.addEventListener('load', resizeCanvas);
+window.addEventListener('resize', resizeCanvas);
+
+
 
 tippy('.link',{
   placement: 'bottom'
